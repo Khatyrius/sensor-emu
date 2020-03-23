deb9a1ed53bbabc02881c2249a13aefa5c6cfc3d
keyjey64 <keyjey64@gmail.com>
2020-03-23 14:36:31 +0100

    Added dao tests

--- app/src/androidTest/java/com/sensor_emu/DaoInstrumentedTest.java
+++ app/src/androidTest/java/com/sensor_emu/DaoInstrumentedTest.java
@@ -6,10 +6,29 @@ import android.content.Context;
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.sensor_emu.dao.configurable.EnergyFrequencyDao;
+import com.sensor_emu.dao.configurable.HumidityFrequencyDao;
+import com.sensor_emu.dao.configurable.PressureFrequencyDao;
+import com.sensor_emu.dao.configurable.TemperatureFrequencyDao;
+import com.sensor_emu.dao.configurable.TimestampDao;
 import com.sensor_emu.dao.measurement.EnergyDao;
+import com.sensor_emu.dao.measurement.HumidityDao;
+import com.sensor_emu.dao.measurement.PressureDao;
+import com.sensor_emu.dao.measurement.TemperatureDao;
 import com.sensor_emu.db.AppDatabase;
+import com.sensor_emu.model.configurable.EnergyFrequency;
+import com.sensor_emu.model.configurable.HumidityFrequency;
+import com.sensor_emu.model.configurable.PressureFrequency;
+import com.sensor_emu.model.configurable.TemperatureFrequency;
+import com.sensor_emu.model.configurable.Timestamp;
 import com.sensor_emu.model.measurement.Energy;
+import com.sensor_emu.model.measurement.Humidity;
+import com.sensor_emu.model.measurement.Pressure;
+import com.sensor_emu.model.measurement.Temperature;
 import com.sensor_emu.utils.TestUtil;
+
+import java.util.Arrays;
 import java.util.List;
 import org.junit.After;
 import org.junit.Before;
@@ -25,6 +44,14 @@ import org.junit.runner.RunWith;
 public class DaoInstrumentedTest {
 
     private EnergyDao energyDao;
+    private HumidityDao humidityDao;
+    private PressureDao pressureDao;
+    private TemperatureDao temperatureDao;
+    private EnergyFrequencyDao energyFrequencyDao;
+    private HumidityFrequencyDao humidityFrequencyDao;
+    private PressureFrequencyDao pressureFrequencyDao;
+    private TemperatureFrequencyDao temperatureFrequencyDao;
+    private TimestampDao timestampDao;
     private AppDatabase db;
 
     @Before
@@ -32,6 +59,14 @@ public class DaoInstrumentedTest {
         Context context = ApplicationProvider.getApplicationContext();
         db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
         energyDao = db.getEnergyDao();
+        humidityDao = db.getHumidityDao();
+        pressureDao = db.getPressureDao();
+        temperatureDao = db.getTemperatureDao();
+        energyFrequencyDao = db.getEnergyFrequencyDao();
+        humidityFrequencyDao = db.getHumidityFrequencyDao();
+        pressureFrequencyDao = db.getPressureFrequencyDao();
+        temperatureFrequencyDao = db.getTemperatureFrequencyDao();
+        timestampDao = db.getTimestampDao();
     }
 
     @After
@@ -40,7 +75,7 @@ public class DaoInstrumentedTest {
     }
 
     @Test
-    public void shouldWriteEnergyAndReadInList() throws Exception {
+    public void shouldInsertEnergyAndGetAllByTimestamp() {
         // given
         final Energy energy = TestUtil.createEnergy(
                 1L, 123,
@@ -53,8 +88,139 @@ public class DaoInstrumentedTest {
         energyDao.insert(energy);
 
         // then
-        final List<Energy> byTimestamp = energyDao.getAllByTimestamp(energy.getTimestamp());
-        assertEquals(byTimestamp.get(0), energy);
+        final List<Energy> result = energyDao.getAllByTimestamp(energy.getTimestamp());
+        assertEquals(result.get(0), energy);
     }
 
+    @Test
+    public void shouldInsertHumidityAndGetAll() {
+        // given
+        final Humidity humidity1 = TestUtil.createHumidity(1L, 123, 456);
+        final Humidity humidity2 = TestUtil.createHumidity(2L, 789, 1000);
+        final List<Humidity> humidityList = Arrays.asList(humidity1, humidity2);
+
+        // when
+        humidityDao.insert(humidity1);
+        humidityDao.insert(humidity2);
+
+        // then
+        final List<Humidity> result = humidityDao.getAll();
+        assertEquals(result, humidityList);
+    }
+
+    @Test
+    public void shouldInsertPressureAndGetById() {
+        // given
+        final Long id = 2L;
+        final Pressure pressure1 = TestUtil.createPressure(1L, 123, 456);
+        final Pressure pressure2 = TestUtil.createPressure(id, 111, 1000);
+
+        // when
+        pressureDao.insert(pressure1);
+        pressureDao.insert(pressure2);
+
+        // then
+        final Pressure result = pressureDao.getById(id);
+        assertEquals(result, pressure2);
+    }
+
+    @Test
+    public void shouldInsertTemperatureAndGetById() {
+        // given
+        final Long id = 1L;
+        final Temperature temperature1 = TestUtil.createTemperature(id, 123, 456);
+        final Temperature temperature2 = TestUtil.createTemperature(2L, 111, 1000);
+
+        // when
+        temperatureDao.insert(temperature1);
+        temperatureDao.insert(temperature2);
+
+        // then
+        final Temperature result = temperatureDao.getById(id);
+        assertEquals(result, temperature1);
+    }
+
+    @Test
+    public void shouldInsertEnergyFrequencyAndGetById() {
+        // given
+        final Long id = 1L;
+        final EnergyFrequency energyFrequency1 = TestUtil.createEnergyFrequency(id, 123, 456);
+        final EnergyFrequency energyFrequency2 = TestUtil.createEnergyFrequency(2L, 111, 1000);
+
+        // when
+        energyFrequencyDao.insert(energyFrequency1);
+        energyFrequencyDao.insert(energyFrequency2);
+
+        // then
+        final EnergyFrequency result = energyFrequencyDao.getById(id);
+        assertEquals(result, energyFrequency1);
+    }
+
+    @Test
+    public void shouldInsertHumidityFrequencyAndGetAll() {
+        // given
+        final HumidityFrequency humidityFrequency1 = TestUtil.createHumidityFrequency(1L, 123, 456);
+        final HumidityFrequency humidityFrequency2 = TestUtil.createHumidityFrequency(2L, 789, 1000);
+        final List<HumidityFrequency> humidityFrequencyList = Arrays.asList(humidityFrequency1, humidityFrequency2);
+
+        // when
+        humidityFrequencyDao.insert(humidityFrequency1);
+        humidityFrequencyDao.insert(humidityFrequency2);
+
+        // then
+        final List<HumidityFrequency> result = humidityFrequencyDao.getAll();
+        assertEquals(result, humidityFrequencyList);
+    }
+
+    @Test
+    public void shouldInsertAndUpdatePressureFrequencyAndGetById() {
+        // given
+        final long id = 2L;
+        final PressureFrequency pressureFrequency1 = TestUtil.createPressureFrequency(1L, 123, 456);
+        final PressureFrequency pressureFrequency2 = TestUtil.createPressureFrequency(id, 789, 1000);
+        final PressureFrequency pressureFrequency3 = TestUtil.createPressureFrequency(id, 111, 3333);
+
+        // when
+        pressureFrequencyDao.insert(pressureFrequency1);
+        pressureFrequencyDao.insert(pressureFrequency2);
+        pressureFrequencyDao.update(pressureFrequency3);
+
+        // then
+        final PressureFrequency result = pressureFrequencyDao.getById(id);
+        assertEquals(result, pressureFrequency3);
+    }
+
+    @Test
+    public void shouldInsertAndDeleteTemperatureFrequency() {
+        // given
+        final TemperatureFrequency temperatureFrequency1 = TestUtil.createTemperatureFrequency(1L, 123, 456);
+        final TemperatureFrequency temperatureFrequency2 = TestUtil.createTemperatureFrequency(2L, 789, 1000);
+        final TemperatureFrequency temperatureFrequency3 = TestUtil.createTemperatureFrequency(3L, 333, 2222);
+
+        final List<TemperatureFrequency> temperatureFrequencyList = Arrays.asList(temperatureFrequency1, temperatureFrequency3);
+
+        // when
+        temperatureFrequencyDao.insert(temperatureFrequency1);
+        temperatureFrequencyDao.insert(temperatureFrequency2);
+        temperatureFrequencyDao.insert(temperatureFrequency3);
+        temperatureFrequencyDao.delete(temperatureFrequency2);
+
+        // then
+        final List<TemperatureFrequency> result = temperatureFrequencyDao.getAll();
+        assertEquals(result, temperatureFrequencyList);
+    }
+
+    @Test
+    public void shouldInsertTimestampAndGetById() {
+        // given
+        final Long id = 1L;
+        final Timestamp timestamp = TestUtil.createTimestamp(id, 123, 456);
+
+        // when
+        timestampDao.insert(timestamp);
+
+        // then
+        final Timestamp result = timestampDao.getById(id);
+        assertEquals(result, timestamp);
+    }
 }
--- app/src/androidTest/java/com/sensor_emu/utils/TestUtil.java
+++ app/src/androidTest/java/com/sensor_emu/utils/TestUtil.java
@@ -1,6 +1,14 @@
 package com.sensor_emu.utils;
 
+import com.sensor_emu.model.configurable.EnergyFrequency;
+import com.sensor_emu.model.configurable.HumidityFrequency;
+import com.sensor_emu.model.configurable.PressureFrequency;
+import com.sensor_emu.model.configurable.TemperatureFrequency;
+import com.sensor_emu.model.configurable.Timestamp;
 import com.sensor_emu.model.measurement.Energy;
+import com.sensor_emu.model.measurement.Humidity;
+import com.sensor_emu.model.measurement.Pressure;
+import com.sensor_emu.model.measurement.Temperature;
 
 public class TestUtil {
 
@@ -23,4 +31,99 @@ public class TestUtil {
         return energy;
     }
 
+    public static Humidity createHumidity(final Long id, final float value, final double timestamp) {
+        final Humidity humidity = new Humidity();
+
+        if(id != null ) {
+            humidity.setId(id);
+        }
+
+        humidity.setValue(value);
+        humidity.setTimestamp(timestamp);
+        return humidity;
+    }
+
+    public static Pressure createPressure(final Long id, final float value, final double timestamp) {
+        final Pressure pressure = new Pressure();
+
+        if(id != null ) {
+            pressure.setId(id);
+        }
+
+        pressure.setValue(value);
+        pressure.setTimestamp(timestamp);
+        return pressure;
+    }
+
+    public static Temperature createTemperature(final Long id, final float value, final double timestamp) {
+        final Temperature temperature = new Temperature();
+
+        if(id != null ) {
+            temperature.setId(id);
+        }
+
+        temperature.setValue(value);
+        temperature.setTimestamp(timestamp);
+        return temperature;
+    }
+
+    public static EnergyFrequency createEnergyFrequency(final Long id, final float value, final double timestamp) {
+        final EnergyFrequency energyFrequency = new EnergyFrequency();
+
+        if(id != null ) {
+            energyFrequency.setId(id);
+        }
+
+        energyFrequency.setValue(value);
+        energyFrequency.setTimestamp(timestamp);
+        return energyFrequency;
+    }
+
+    public static HumidityFrequency createHumidityFrequency(final Long id, final float value, final double timestamp) {
+        final HumidityFrequency humidityFrequency = new HumidityFrequency();
+
+        if(id != null ) {
+            humidityFrequency.setId(id);
+        }
+
+        humidityFrequency.setValue(value);
+        humidityFrequency.setTimestamp(timestamp);
+        return humidityFrequency;
+    }
+
+    public static PressureFrequency createPressureFrequency(final Long id, final float value, final double timestamp) {
+        final PressureFrequency pressureFrequency = new PressureFrequency();
+
+        if(id != null ) {
+            pressureFrequency.setId(id);
+        }
+
+        pressureFrequency.setValue(value);
+        pressureFrequency.setTimestamp(timestamp);
+        return pressureFrequency;
+    }
+
+    public static TemperatureFrequency createTemperatureFrequency(final Long id, final float value, final double timestamp) {
+        final TemperatureFrequency temperatureFrequency = new TemperatureFrequency();
+
+        if(id != null ) {
+            temperatureFrequency.setId(id);
+        }
+
+        temperatureFrequency.setValue(value);
+        temperatureFrequency.setTimestamp(timestamp);
+        return temperatureFrequency;
+    }
+
+    public static Timestamp createTimestamp(final Long id, final float value, final double timestamp) {
+        final Timestamp ts = new Timestamp();
+
+        if(id != null ) {
+            ts.setId(id);
+        }
+
+        ts.setValue(value);
+        ts.setTimestamp(timestamp);
+        return ts;
+    }
 }
